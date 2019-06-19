709b03b4584b80294f7a0b3ba8b2996c88db238d
Yago Lopes <yagolopeslazaro@gmail.com>
2019-06-19 09:01:48 -0300

    correção ortográfica na modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,47 +17,39 @@ export default function Header() {
             <strong>Fragata</strong> corp.
           </p>
         </div>
-        <p id="saibamais" onClick={OpenOrCloseModal}>
+        <p style={{ cursor: "pointer" }} onClick={OpenOrCloseModal}>
           Saiba mais
         </p>
       </div>
 
-      <Modal open={modal} onClose={OpenOrCloseModal} center>
-        <div className="panel panel-default">
-          <div className="panel-body">
-            <div className="text-center">
-              <h3>
-                <i className="fa fa-lock fa-4x" />
-              </h3>
-              <h2 className="text-center">Por que acreditar?</h2>
-              <br />
-              <div className="panel-body">
-                <p>
-                  O Fragata Corp pretende alcançar diversos públicos e
-                  objetivos, porém se tivéssemos que resumir nossa missão, com
-                  certeza o termo utilizado seria{" "}
-                  <strong>ecossistema tecnológico.</strong> Não temos dúvidas
-                  sobre a constante evolução de demanda da tecnologia, porém
-                  algo que nos preocupa é falta de informação que a sociedade
-                  tem quanto a esse assunto.
-                </p>{" "}
-                <br />
-                <p>
-                  Buscamos através de parceiros uma oportunidade capaz de levar
-                  conhecimento perante a população, visamos alcançar Escolas,
-                  Empresas, Instituições Privadas e Públicas, e ofertar a eles
-                  um minicurso de 4 horas com o objetivo de desenvolver uma
-                  aplicação do zero, que tenha pertinência com o dia a dia dos
-                  interessados.
-                </p>
-                <br />
-                <p>
-                  Nessa deixa pretendemos apresentar a eles produtos e
-                  oportunidades que o mercado tecnológico oferece.
-                </p>
-              </div>
-            </div>
-          </div>
+      <Modal id="modal" open={modal} onClose={OpenOrCloseModal} center>
+        <div>
+          <h3>
+            <i />
+          </h3>
+          <h2>Por que acreditar?</h2>
+          <br />
+          <p>
+            O Fragata Corp pretende alcançar diversos públicos e objetivos,
+            porém se tivéssemos que resumir nossa missão, com certeza o termo
+            utilizado seria <strong>ecossistema tecnológico.</strong> Não temos
+            dúvidas sobre a constante evolução de demanda tecnológica, porém
+            algo que nos preocupa é falta de informação que a sociedade tem
+            quanto a esse assunto.
+          </p>{" "}
+          <br />
+          <p>
+            Buscamos através de parceiros uma oportunidade capaz de levar
+            conhecimento perante a população, visamos alcançar Escolas,
+            Empresas, Instituições Privadas e Públicas, e ofertar a eles um
+            minicurso de 4 horas com o objetivo de desenvolver uma aplicação do
+            zero, que tenha pertinência com o dia a dia dos interessados.
+          </p>
+          <br />
+          <p>
+            Nessa deixa pretendemos apresentar a eles produtos e oportunidades
+            que o mercado tecnológico oferece.
+          </p>
         </div>
       </Modal>
     </header>
