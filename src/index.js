cdd5b0f96898d4ab735216305ac0ff7a6f36c72f
Yago Lopes <yagolopeslazaro@gmail.com>
2019-06-21 13:05:26 -0300

    add eslint stylegid arbnb

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./pages/App";
 import "./global.css";
 import "./animate.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, global.document.getElementById("root"));
